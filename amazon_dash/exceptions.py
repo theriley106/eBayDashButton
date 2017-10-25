1c2209d03b988c8a3b10ad237f2406ad2d604b80
Theriley106 <christopherlambert106@gmail.com>
2017-10-24 23:50:16 -0400

    Completed repo" git psuh

--- amazon_dash/exceptions.py
+++
@@ -1,4 +0,0 @@
-
-
-class SecurityException(Exception):
-    pass
--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-import requests
-import bs4
+import os
 
-#def goToURL(itemNum):
-
-class eBay(object):
-	def __init__(self):
-		self.session = requests.session()
+if __name__ == "__main__":
+	if os.path.isfile("amazon-dash.yml") == False:
+		print("You haven't created a valid config file yet...")
+		if raw_input("Create one now? Y/N ").upper() == "Y":
+			mac = raw_input("Dash Button Mac Address: ")
+			delay = raw_input("Delay between button press: ")
+			itemNum = raw_input("Item Number: ")
+			file = open("amazon-dash.yml", "w")
+			'''
+			settings:
+			  delay: {}
+			devices:
+			  {}:
+			    name: test
+			    user: test
+			    cmd: python interactions.py {}
+			    '''.format(delay, mac, itemNum)
+			file.write(config) 
+			file.close()
+			os.system("sudo chown root:root amazon-dash.yml")
+			os.system("sudo chmod 700 amazon-dash.yml")
+	try:
+		os.system("sudo amazon-dash run")
+	except:
+		Exception('It looks like you don\'t have Amazon Dash Installed - run "sudo pip install amazon-dash"')
+			
